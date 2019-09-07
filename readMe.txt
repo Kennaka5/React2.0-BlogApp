774b0af2fa09cce44b927da538c870face093d47
Ken Nakayama <kennaka5@gmail.com>
2019-09-06 19:56:43 -0700

    working content initial commit

---
+++ readMe.txt
@@ -0,0 +1,5 @@
+Below is a timeline of how to build this apps boiler plate mock up
+
+1. in terminal run 'npx expo-cli init "blog name"'
+2. go through the prompts, leave the template blank and name the project
+3. install react navigation 'npm install react navigation'
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,33 @@
+import React, {useContext} from 'react';
+import {View, Text, StyleSheet, FlatList, Button} from 'react-native';
+import BlogContext from '../context/BlogContext';
+
+
+
+const IndexScreen = () => {
+    const {data, addBlogPost} = useContext(BlogContext)
+
+    return(
+        <>
+        <Text>Index Screen</Text>
+        <Button title="add Post" onPress={addBlogPost}/>
+        <FlatList 
+            data={data}
+            keyExtractor={(blogPost) => blogPost.title}
+            renderItem={({ item }) => {
+                return(
+                    <Text>{item.title}</Text>
+                )
+
+            }}
+
+        />
+        </>
+    )
+};
+
+const style = StyleSheet.create({
+
+});
+
+export default IndexScreen
